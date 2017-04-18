a34175e1e2f16f2c58e6f286dae0479d6525572e
Fung <fython@163.com>
2017-04-18 18:30:19 +0800

    Revert "Remove Baidu Result"
    This reverts commit 700fa7ff8ed617f7232692804587735d46ea18b8.

--- bot_main.py
+++ bot_main.py
@@ -205,7 +205,7 @@ def search_photo_and_reply(message, pic):
         downloaded_file = bot.download_file(file_info.file_path)
         with open(path, 'wb') as new_file:
             new_file.write(downloaded_file)
-        bot.send_message(message.chat.id, '搜图快速入口：\n<a herf=\"{0}\">Google</a>'.format(bot_utils.get_google_search_image(public_url)), reply_to_message_id=message.message_id, disable_web_page_preview = True, parse_mode = 'HTML')
+        bot.send_message(message.chat.id, '搜图快速入口：\n<a herf=\"{0}\">Google</a>\n<a href=\"{1}\">Baidu</a>'.format(bot_utils.get_google_search_image(public_url), bot_utils.get_baidu_search_image(public_url)), reply_to_message_id=message.message_id, disable_web_page_preview = True, parse_mode = 'HTML')
     except:
         traceback.print_exc()
         bot.reply_to(message, '出现了点意外，等会再试试吧……')
